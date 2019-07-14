6dcf9bcf66f2a3acedc91f9bd0dd0b948db362c0
Sagie501 <Sagie012@gmail.com>
2019-07-14 11:52:03 +0300

    Add files via upload

---
+++ Singleton/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns_20190714
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton s1 = Singleton.getInstance();
+            Singleton s2 = Singleton.getInstance();
+            Singleton s3 = Singleton.getInstance();
+
+            s1.printCurrentTime();
+            s2.printCurrentTime();
+            s3.printCurrentTime();
+        }
+    }
+}
---
+++ Singleton/Singleton.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns_20190714
+{
+    /// <summary>
+    /// This is the class made for the singleton design pattern. I made this for fun. 
+    /// <para>
+    /// Toaster is the nickname for a loading animation. 
+    /// </para>
+    /// </summary>
+    class Singleton
+    {
+        private static Singleton instance;
+
+        private static readonly object key = new object();
+
+        private Singleton()
+        {
+
+        }
+
+        public static Singleton getInstance()
+        {
+            if (null != instance)
+                return instance;
+            lock (key)
+            {
+                if (null == instance)
+                    instance = new Singleton();
+            }
+            return instance;
+        }
+
+        public void printCurrentTime()
+        {
+            Console.WriteLine("The time now is: " + DateTime.Now.ToString("HH:mm:ss tt"));
+        }
+    }
+}
